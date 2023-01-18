41fcb9559178f424fe2fe54a6c27ed3d4d5edaa8
Steve <playzzsteven@gmail.com>
2023-01-18 17:19:19 -0500

    Part 2 - Fix forge client crashes :)

--- common/src/main/java/com/jellyfishing/client/JellyfishingClient.java
+++ common/src/main/java/com/jellyfishing/client/JellyfishingClient.java
@@ -1,5 +1,6 @@
 package com.jellyfishing.client;
 
+import com.google.common.collect.ImmutableList;
 import com.jellyfishing.client.model.AbstractJellyfishModel;
 import com.jellyfishing.client.model.ModelPerspectiveRegistry;
 import com.jellyfishing.client.model.SpatulaModel;
@@ -31,38 +32,30 @@ public class JellyfishingClient {
         EntityRendererRegistry.register(JellyfishingEntities.JELLYFISH, JellyfishRenderer::new);
         EntityRendererRegistry.register(JellyfishingEntities.BLUE_JELLYFISH, BlueJellyfishRenderer::new);
 
-        RenderTypeRegistry.register(RenderType.translucent(),
-                JellyfishingBlocks.JELLY_BLOCK.get(),
-                JellyfishingBlocks.BLUE_JELLY_BLOCK.get(),
-                JellyfishingBlocks.BUBBLE_BLOCK.get()
-        );
-        RenderTypeRegistry.register(RenderType.cutoutMipped(),
-                JellyfishingBlocks.CORAL_PLANT.get(),
-                JellyfishingBlocks.TUBE_PLANT.get(),
-                JellyfishingBlocks.SEANUT_BUSH.get(),
-                JellyfishingBlocks.PINEAPPLE_PLANT.get(),
-                JellyfishingBlocks.POTTED_PINEAPPLE.get(),
-                JellyfishingBlocks.SCRAP_METAL_WINDOW.get(),
-                JellyfishingBlocks.CHROME_DOOR.get(),
-                JellyfishingBlocks.CHROME_VENT.get()
-        );
+        var blockRendersTranslucent = ImmutableList.of(JellyfishingBlocks.JELLY_BLOCK, JellyfishingBlocks.BLUE_JELLY_BLOCK);
+        var blockRendersMipped = ImmutableList.of(JellyfishingBlocks.CORAL_PLANT, JellyfishingBlocks.TUBE_PLANT, JellyfishingBlocks.SEANUT_BUSH, JellyfishingBlocks.PINEAPPLE_PLANT, JellyfishingBlocks.POTTED_PINEAPPLE, JellyfishingBlocks.SCRAP_METAL_WINDOW, JellyfishingBlocks.CHROME_DOOR, JellyfishingBlocks.CHROME_VENT);
+
+        blockRendersTranslucent.forEach(block -> block.listen((real)-> RenderTypeRegistry.register(RenderType.translucent(), real)));
+        blockRendersMipped.forEach(block -> block.listen((real)-> RenderTypeRegistry.register(RenderType.cutoutMipped(), real)));
 
         ParticleProviderRegistry.register(JellyfishingParticles.CLOUD_PARTICLE, CloudParticle.Factory::new);
 
-        ArmorRenderer.register((matrices, vertexConsumers, stack, entity, slot, light, contextModel) -> {
-            SuitModel suitModel = new SuitModel(Minecraft.getInstance().getEntityModels().bakeLayer(SuitModel.LAYER_LOCATION));
-            suitModel.setCurrentEntity(entity);
-            contextModel.copyPropertiesTo(suitModel);
-            suitModel.setAllVisible(true);
-            if (stack.getItem().equals(JellyfishingItems.AIR_SUIT_HELMET.get()))
-                ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, suitModel, Jellyfishing.id("textures/models/armor/air_suit_helmet.png"));
-            else if (stack.getItem().equals(JellyfishingItems.AIR_SUIT_CHESTPLATE.get()))
-                ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, suitModel, Jellyfishing.id("textures/models/armor/air_suit_chest.png"));
-            else if (stack.getItem().equals(JellyfishingItems.AIR_SUIT_LEGGINGS.get()))
-                ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, suitModel, Jellyfishing.id("textures/models/armor/air_suit_legs.png"));
-            else if (stack.getItem().equals(JellyfishingItems.AIR_SUIT_BOOTS.get()))
-                ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, suitModel, Jellyfishing.id("textures/models/armor/air_suit_boots.png"));
-        }, JellyfishingItems.AIR_SUIT_HELMET.get(), JellyfishingItems.AIR_SUIT_CHESTPLATE.get(), JellyfishingItems.AIR_SUIT_LEGGINGS.get(), JellyfishingItems.AIR_SUIT_BOOTS.get());
+        JellyfishingItems.AIR_SUIT_HELMET.listen((item)-> JellyfishingItems.AIR_SUIT_CHESTPLATE.listen((item1)-> JellyfishingItems.AIR_SUIT_LEGGINGS.listen((item2)-> JellyfishingItems.AIR_SUIT_BOOTS.listen((item3)->
+                ArmorRenderer.register((matrices, vertexConsumers, stack, entity, slot, light, contextModel) -> {
+                    SuitModel suitModel = new SuitModel(Minecraft.getInstance().getEntityModels().bakeLayer(SuitModel.LAYER_LOCATION));
+                    suitModel.setCurrentEntity(entity);
+                    contextModel.copyPropertiesTo(suitModel);
+                    suitModel.setAllVisible(true);
+                    if (stack.getItem().equals(JellyfishingItems.AIR_SUIT_HELMET))
+                        ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, suitModel, Jellyfishing.id("textures/models/armor/air_suit_helmet.png"));
+                    else if (stack.getItem().equals(JellyfishingItems.AIR_SUIT_CHESTPLATE))
+                        ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, suitModel, Jellyfishing.id("textures/models/armor/air_suit_chest.png"));
+                    else if (stack.getItem().equals(JellyfishingItems.AIR_SUIT_LEGGINGS))
+                        ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, suitModel, Jellyfishing.id("textures/models/armor/air_suit_legs.png"));
+                    else if (stack.getItem().equals(JellyfishingItems.AIR_SUIT_BOOTS))
+                        ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, suitModel, Jellyfishing.id("textures/models/armor/air_suit_boots.png"));
+        }, item, item1, item2, item3)))));
+
 
         ModelPerspectiveRegistry.registerModelProvider(new ModelResourceLocation(Jellyfishing.id("spatula_in_hand"), "inventory"));
         ModelPerspectiveRegistry.registerModelProvider(new ModelResourceLocation(Jellyfishing.id("golden_spatula_in_hand"), "inventory"));
@@ -72,10 +65,10 @@ public class JellyfishingClient {
         ModelPerspectiveRegistry.registerModelProvider(new ModelResourceLocation(Jellyfishing.id("master_karate_glove_right"), "inventory"));
         ModelPerspectiveRegistry.registerModelProvider(new ModelResourceLocation(Jellyfishing.id("power_karate_glove_right"), "inventory"));
         ModelPerspectiveRegistry.registerModelProvider(new ModelResourceLocation(Jellyfishing.id("power_karate_glove_left"), "inventory"));
-        ModelPerspectiveRegistry.registerMultiplePerspective(JellyfishingItems.SPATULA.get(), new ModelResourceLocation(Jellyfishing.id("spatula_in_hand"), "inventory"), new ModelResourceLocation(Jellyfishing.id("spatula"), "inventory"));
-        ModelPerspectiveRegistry.registerMultiplePerspective(JellyfishingItems.GOLDEN_SPATULA.get(), new ModelResourceLocation(Jellyfishing.id("golden_spatula_in_hand"), "inventory"), new ModelResourceLocation(Jellyfishing.id("golden_spatula"), "inventory"));
-        ModelPerspectiveRegistry.registerLeftRightPerspective(JellyfishingItems.KARATE_GLOVE.get(), new ModelResourceLocation(Jellyfishing.id("karate_glove_left"), "inventory"), new ModelResourceLocation(Jellyfishing.id("karate_glove_right"), "inventory"), new ModelResourceLocation(Jellyfishing.id("karate_glove"), "inventory"));
-        ModelPerspectiveRegistry.registerLeftRightPerspective(JellyfishingItems.MASTER_KARATE_GLOVE.get(), new ModelResourceLocation(Jellyfishing.id("master_karate_glove_left"), "inventory"), new ModelResourceLocation(Jellyfishing.id("master_karate_glove_right"), "inventory"), new ModelResourceLocation(Jellyfishing.id("master_karate_glove"), "inventory"));
-        ModelPerspectiveRegistry.registerLeftRightPerspective(JellyfishingItems.POWER_KARATE_GLOVE.get(), new ModelResourceLocation(Jellyfishing.id("power_karate_glove_left"), "inventory"), new ModelResourceLocation(Jellyfishing.id("power_karate_glove_right"), "inventory"), new ModelResourceLocation(Jellyfishing.id("power_karate_glove"), "inventory"));
+        JellyfishingItems.SPATULA.listen((item)-> ModelPerspectiveRegistry.registerMultiplePerspective(item, new ModelResourceLocation(Jellyfishing.id("spatula_in_hand"), "inventory"), new ModelResourceLocation(Jellyfishing.id("spatula"), "inventory")));
+        JellyfishingItems.GOLDEN_SPATULA.listen((item)-> ModelPerspectiveRegistry.registerMultiplePerspective(item, new ModelResourceLocation(Jellyfishing.id("golden_spatula_in_hand"), "inventory"), new ModelResourceLocation(Jellyfishing.id("golden_spatula"), "inventory")));
+        JellyfishingItems.KARATE_GLOVE.listen((item)-> ModelPerspectiveRegistry.registerLeftRightPerspective(item, new ModelResourceLocation(Jellyfishing.id("karate_glove_left"), "inventory"), new ModelResourceLocation(Jellyfishing.id("karate_glove_right"), "inventory"), new ModelResourceLocation(Jellyfishing.id("karate_glove"), "inventory")));
+        JellyfishingItems.MASTER_KARATE_GLOVE.listen((item)-> ModelPerspectiveRegistry.registerLeftRightPerspective(item, new ModelResourceLocation(Jellyfishing.id("master_karate_glove_left"), "inventory"), new ModelResourceLocation(Jellyfishing.id("master_karate_glove_right"), "inventory"), new ModelResourceLocation(Jellyfishing.id("master_karate_glove"), "inventory")));
+        JellyfishingItems.POWER_KARATE_GLOVE.listen((item)-> ModelPerspectiveRegistry.registerLeftRightPerspective(item, new ModelResourceLocation(Jellyfishing.id("power_karate_glove_left"), "inventory"), new ModelResourceLocation(Jellyfishing.id("power_karate_glove_right"), "inventory"), new ModelResourceLocation(Jellyfishing.id("power_karate_glove"), "inventory")));
     }
 }
--- forge/src/main/java/com/jellyfishing/core/forge/JellyfishingForge.java
+++ forge/src/main/java/com/jellyfishing/core/forge/JellyfishingForge.java
@@ -1,5 +1,6 @@
 package com.jellyfishing.core.forge;
 
+import com.jellyfishing.client.JellyfishingClient;
 import com.jellyfishing.common.worldgen.JellyfishFieldsSurfaceRuleData;
 import com.jellyfishing.common.worldgen.JellyfishingBiomeRegion;
 import com.jellyfishing.core.Jellyfishing;
@@ -7,6 +8,8 @@ import com.jellyfishing.core.registry.JellyfishingExtras;
 import dev.architectury.forge.ArchitecturyForge;
 import dev.architectury.platform.forge.EventBuses;
 import dev.architectury.registry.level.entity.forge.EntityAttributeRegistryImpl;
+import dev.architectury.utils.Env;
+import dev.architectury.utils.EnvExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -19,6 +22,7 @@ public class JellyfishingForge {
         var bus = FMLJavaModLoadingContext.get().getModEventBus();
         EventBuses.registerModEventBus(Jellyfishing.MOD_ID, bus);
         Jellyfishing.init();
+        EnvExecutor.runInEnv(Env.CLIENT, ()-> JellyfishingClient::onClientInit);
 
         bus.addListener(this::commonSetup);
     }
